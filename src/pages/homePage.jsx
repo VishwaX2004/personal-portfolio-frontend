34b2e8a2a3de5eca44b7b3e7f5721671deaec7c6
Vishwa_Pramuditha <vishwapramuditha200@gmail.com>
2026-09-13 17:17:22 +0530

    fix: Correct import order and remove unnecessary whitespace in HomePage component

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,7 +1,5 @@
 import { Toaster } from "react-hot-toast";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 import Hero from "../components/hero";
 import About from "../components/about";
@@ -9,6 +7,8 @@ import Skills from "../components/skills";
 import Experience from "../components/experience";
 import Projects from "../components/projects";
 import Contact from "../components/contacts";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -63,7 +63,7 @@ export default function Home() {
 
       </main>
 
-      <Footer />
+      <Footer/>
 
     </div>
   );
